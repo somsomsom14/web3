f5323edfe22c0ed2a4e81caa7e59264627ae4a38
somsomsom14 <145942556+somsomsom14@users.noreply.github.com>
2023-10-17 02:26:32 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import AppBasic01 from './AppBasic01';
+import AppBasic02 from './AppBasic02';
+import AppBasic03 from './AppBasic03';
+import AppBasic04 from './AppBasic04';
+import AppBasic05 from './AppBasic05';
+import AppBasic06 from './AppBasic06';
+import AppBasic07 from './AppBasic07';
+import AppBasic08 from './AppBasic08';
+import AppBasic09 from './AppBasic09';
+
+export default function App() {
+  return <AppBasic09 />;
+}
